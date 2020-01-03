86719580d128c5243d61a77a06db0307c8bc372c
bhoomikag203 <bhoomikag203@gmail.com>
2020-01-04 00:20:34 +0530

    Styling campgrounds

--- app.js
+++ app.js
@@ -21,7 +21,33 @@ var campgrounds = [{
     {
         name: "Dark Hill",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtUc0GWE8NI2WQOOdRV-SUddqQMd2zjaHSVW9IvMExkJzvTmJeaQ&s"
+    },
+    {
+        name: "The Great Hills",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBe-BrfrXaf-3F0YjaPzB8sMnaIhINDd1GvQsY3fX7QFn-_SYvow&s"
+    },
+    {
+        name: "The Mountians",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv2RQ7Vzt_7N3FLiDsWFaXScY17PA0ffrJg-4SU0l2KVRIEMzd&s"
+    },
+    {
+        name: "Dark Hill",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtUc0GWE8NI2WQOOdRV-SUddqQMd2zjaHSVW9IvMExkJzvTmJeaQ&s"
+    },
+    {
+        name: "The Great Hills",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBe-BrfrXaf-3F0YjaPzB8sMnaIhINDd1GvQsY3fX7QFn-_SYvow&s"
+    },
+    {
+        name: "The Mountians",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv2RQ7Vzt_7N3FLiDsWFaXScY17PA0ffrJg-4SU0l2KVRIEMzd&s"
+    },
+    {
+        name: "Dark Hill",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtUc0GWE8NI2WQOOdRV-SUddqQMd2zjaHSVW9IvMExkJzvTmJeaQ&s"
     }
+
+
 ];
 
 app.get('/campgrounds', (req, res) => {
